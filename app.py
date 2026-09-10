d4b249709ed66b156a9e3ada783f04d52f48ef78
hasinimahi2006 <hasinimahi77@gmail.com>
2026-09-10 14:37:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/')
 def hello():
-    return "helloworlddddd devops"
+    return "helloworlddddd devopsss"
 if __name__=='__main__':
     app.run(debug=True)
